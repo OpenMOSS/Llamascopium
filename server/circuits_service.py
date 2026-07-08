835d4ab819e23f731018de23089f462c0860c09e
JacklE0niden <243981062@qq.com>
2026-07-08 14:22:11 +0800

    autointerp score output

--- server/circuits_service.py
+++ server/circuits_service.py
@@ -733,7 +733,7 @@ def run_circuit_trace(
     max_feature_nodes: int = DEFAULT_MAX_FEATURE_NODES,
     batch_size: int = 1,
     order_mode: str = "abs",
-    mongo_uri: str = "mongodb://10.244.36.152:27017",
+    mongo_uri: str = "mongodb://10.246.56.57:27017",
     mongo_db: str = "mechinterp",
     sae_series: str = "BT4-exp128",
     act_times_max: Optional[int] = None,
@@ -937,7 +937,7 @@ def main():
                        choices=["positive", "negative", "move_pair", "group"],
                        help="Sorting mode")
     
-    parser.add_argument("--mongo_uri", type=str, default="mongodb://10.244.36.152:27017",
+    parser.add_argument("--mongo_uri", type=str, default="mongodb://10.246.56.57:27017",
                        help="MongoDB URI")
     parser.add_argument("--mongo_db", type=str, default="mechinterp",
                        help="MongoDB database name")
